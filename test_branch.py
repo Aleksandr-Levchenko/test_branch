1782de6d0768aecf188aaba5722db888cd36f2d6
Aleksandr Levchenko <54205915+Aleksandr-Levchenko@users.noreply.github.com>
2023-06-11 20:43:46 +0300

    Add function Sum

--- test_branch.py
+++ test_branch.py
@@ -2,7 +2,16 @@
 def main():
     # This is version # 1
     ver = "This is version of programm # 1"
+    
+    a = int(input(f"Input a >> "))
+    b = int(input(f"Input b >> "))
+    print(f"sum = {sum(a, b)}")
+    
     return f"{ver}"
+
+
+def sum(a, b: int) ->int:
+    return a + b
 
 if __name__ == "__main__":
     print(main())
